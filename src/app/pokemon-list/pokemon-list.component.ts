d3512972cea6397fba137f97af75356d4565783a
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2022-06-12 18:14:09 -0300

    Programming Finished

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PokemonService } from 'src/_services/pokemon.service';
 import { Pokemon } from '../../_model/pokemon';
 import { Type } from '../../_model/type';
 
@@ -8,36 +9,9 @@ import { Type } from '../../_model/type';
   styleUrls: ['./pokemon-list.component.sass']
 })
 export class PokemonListComponent  {
+  
+  constructor (public pokemonService: PokemonService) {
 
-  public pokemons: Pokemon[] = [
-    {
-      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
-      number: 1,
-      name: 'Bulbassaur',
-      types: [
-        Type.Grass,
-        Type.Poison
-      ] 
-    },
-    {
-      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png',
-      number: 2,
-      name: 'Ivyssaur',
-      types: [
-        Type.Grass,
-        Type.Poison
-      ] 
-    },
-    {
-      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png',
-      number: 3,
-      name: 'Venossaur',
-      types: [
-        Type.Grass,
-        Type.Poison,
-        Type.Fire
-      ] 
-    }
-  ];
+  }
 
 }
